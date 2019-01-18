f17475b798ee84566b595150fe7b168eb5ca8160
Sankar Prasad Biswas <sankar@collegify.com>
2019-01-18 14:48:51 +0530

    project with tutorial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Layout from '../component/layout';
+
+
+
+const About = () => (
+    <Layout>
+        <div>
+
+            <h1>next js About prize</h1>
+            <p>Application to view about page</p>
+
+        </div>
+    </Layout>
+
+);
+
+
+
+export default About;
